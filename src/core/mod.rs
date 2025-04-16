4599d87d6113ec1f3d308e8fddb63036bf78aef9
Anirudh Vyas <av@goldilocksfashion>
2025-04-16 10:03:26 -0700

    tracing library changes to formatter to display better diagnostics

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -7,20 +7,7 @@ pub mod producer;
 pub mod storage;
 pub mod storage_meta;
 pub mod threads;
+
 pub trait XaeroData: Any + Send + Sync + bincode::Decode<()> + bincode::Encode {}
 
-impl XaeroData for Vec<u8> {}
-impl XaeroData for String {}
-impl XaeroData for i32 {}
-impl XaeroData for i64 {}
-impl XaeroData for u32 {}
-impl XaeroData for u64 {}
-impl XaeroData for f32 {}
-impl XaeroData for f64 {}
-impl XaeroData for bool {}
-impl XaeroData for () {}
-impl XaeroData for [u8; 32] {}
-impl XaeroData for [u8; 64] {}
-impl XaeroData for [u8; 128] {}
-impl XaeroData for [u8; 256] {}
-impl XaeroData for [u8; 512] {}
+impl<T> XaeroData for T where T: Any + Send + Sync + bincode::Decode<()> + bincode::Encode {}
