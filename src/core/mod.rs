48dbf1c2774db6a6b1c29ec3f35d458e6733e802
Anirudh Vyas <av@goldilocksfashion>
2025-04-15 19:18:43 -0700

    Basic merkle tree test passes for 4 leaves or pieces of data added, also added debugging hashes for better fmt and display

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -8,3 +8,19 @@ pub mod storage;
 pub mod storage_meta;
 pub mod threads;
 pub trait XaeroData: Any + Send + Sync + bincode::Decode<()> + bincode::Encode {}
+
+impl XaeroData for Vec<u8> {}
+impl XaeroData for String {}
+impl XaeroData for i32 {}
+impl XaeroData for i64 {}
+impl XaeroData for u32 {}
+impl XaeroData for u64 {}
+impl XaeroData for f32 {}
+impl XaeroData for f64 {}
+impl XaeroData for bool {}
+impl XaeroData for () {}
+impl XaeroData for [u8; 32] {}
+impl XaeroData for [u8; 64] {}
+impl XaeroData for [u8; 128] {}
+impl XaeroData for [u8; 256] {}
+impl XaeroData for [u8; 512] {}
