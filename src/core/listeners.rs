468bd82d1f284be20cf81b81bb8d634b2c96a540
Anirudh Vyas <av@goldilocksfashion>
2025-04-20 11:14:59 -0700

    Thread pool setup for listeners

--- src/core/listeners.rs
+++ src/core/listeners.rs
@@ -1,8 +1,9 @@
-use std::{collections::HashMap, sync::Arc, thread::JoinHandle};
+use std::{collections::BTreeMap, sync::Arc, thread::JoinHandle};
 
 use crossbeam::channel::{Sender, *};
+use libp2p::futures::executor::ThreadPool;
 
-use super::{XaeroData, event::Event};
+use super::{CONF, XaeroData, event::Event};
 
 /// Event listener forms the crux of our event plumbing.
 /// Mostly event listener is bound to a thread-pool and is organized in a tree like
@@ -12,44 +13,95 @@ pub struct EventListener<T>
 where
     T: XaeroData + 'static,
 {
-    pub id: u64,
+    id: Option<[u8; 32]>, // auto-generated
+    pub address: Option<String>,
+    pub inbox: Sender<Event<T>>,
     pub rx: Receiver<Event<T>>,
-    children: HashMap<JoinHandle<()>, Sender<Event<T>>>,
+    pub pool: ThreadPool,
     pub handler: Arc<dyn Fn(Event<T>) + Send + Sync>,
 }
+pub trait VersioningScheme {
+    fn emit_version(&self, seed_name: &str, seed_group: &str) -> String;
+}
+impl<T> VersioningScheme for EventListener<T>
+where
+    T: XaeroData + 'static,
+{
+    fn emit_version(&self, seed_name: &str, seed_group: &str) -> String {
+        format!(
+            "{}_{}_{}",
+            seed_group,
+            seed_name,
+            std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .expect("Time went backwards")
+                .as_micros() as u64
+        )
+    }
+}
+pub trait AddressingScheme {
+    fn emit_address(&self, seed_name: &str, seed_group: &str) -> String;
+}
 
+impl<T> AddressingScheme for EventListener<T>
+where
+    T: XaeroData,
+{
+    fn emit_address(&self, seed_name: &str, seed_group: &str) -> String {
+        format!(
+            "{}/{}/{}/{}",
+            seed_group
+                .to_lowercase()
+                .replace(" ", "_")
+                .replace("-", "_"),
+            seed_name.to_lowercase().replace(" ", "_").replace("-", "_"),
+            self.emit_version(seed_name, seed_group),
+            crate::indexing::hash::sha_256::<String>(&seed_name.to_string())
+                .to_vec()
+                .iter()
+                .map(|x| format!("{:02x}", x))
+                .collect::<String>()
+                .to_lowercase()
+                .replace("-", "_")
+        )
+    }
+}
 impl<T> EventListener<T>
 where
     T: XaeroData + 'static,
 {
-    pub fn new(
-        id: u64,
-        rx: Receiver<Event<T>>,
-        children: HashMap<JoinHandle<()>, Sender<Event<T>>>,
-        handler: Arc<dyn Fn(Event<T>) + Send + Sync>,
-    ) -> Self {
+    pub fn new(name: &str, group: String, handler: Arc<dyn Fn(Event<T>) + Send + Sync>) -> Self {
+        // TODO: versioning scheme duplication - REMOVE
+        let v = format!(
+            "{}_{}_{}",
+            group,
+            name,
+            std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .expect("Time went backwards")
+                .as_micros() as u64
+        );
+        let (tx, rx) = crossbeam::channel::unbounded();
+        let id = crate::indexing::hash::sha_256::<String>(&String::from(name))
+            .try_into()
+            .expect("failed to convert hash");
+        let tp = ThreadPool::builder()
+            .pool_size(
+                CONF.get()
+                    .expect("failed to load config")
+                    .threads
+                    .num_worker_threads,
+            )
+            .name_prefix(format!("xaeroflux-event-listener-{}", hex::encode(id.unwrap_or_default())))
+            .create()
+            .expect("failed to create thread pool");
         EventListener {
             id,
-            children,
+            address: None,
+            inbox: tx,
             rx,
+            pool: tp,
             handler,
         }
     }
-}
-
-pub fn bootstrap_listener<T>(
-    id: u64,
-    rx: Receiver<Event<T>>,
-    children: Vec<Sender<Event<T>>>,
-    handler: Arc<dyn Fn(Event<T>) + Send + Sync>,
-) -> EventListener<T>
-where
-    T: XaeroData + 'static,
-{
-    let j = std::thread::spawn(move || {
-        while let Ok(event) = rx.recv() {
-            (handler)(event);
-        }
-    });
-    EventListener::new(id, rx, children, handler)
 }
