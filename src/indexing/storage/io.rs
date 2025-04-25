4860766fda52ba012e774302935464827edab602
Anirudh Vyas <av@goldilocksfashion>
2025-04-24 17:08:51 -0700

    clippy fix for error mapping file panic with variables without format! macro'

--- src/indexing/storage/io.rs
+++
@@ -1,7 +0,0 @@
-use crate::indexing::storage::format::XaeroOnDiskPage;
-
-pub trait XaeroMerklePageCache {
-    fn read_page(&mut self, idx: usize) -> anyhow::Result<XaeroOnDiskPage>;
-    fn write_page(&mut self, page: &XaeroOnDiskPage) -> anyhow::Result<()>;
-    fn flush(&mut self) -> anyhow::Result<()>;
-}
